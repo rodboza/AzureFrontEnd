caabc2972cc933dbb9731091a74955282fedd5fd
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-17 15:29:00 -0300

    Create consts.js

---
+++ app/config/consts.js
@@ -0,0 +1,12 @@
+angular.module('primeiraApp').constant('consts', {
+  _appName: 'RODBOZA - Frontend APP',
+  _version: '1.0',
+  _owner: 'RODBOZA',
+  _year: '2017',
+  _site: 'http://rodboza.azurewebsites.net/',
+  _apiUrl: 'http://rodboza.backend.azurewebsites.net/api',
+  _oapiUrl: 'http://rodboza.backend.azurewebsites.net/oapi',
+  _userKey: '_primeira_app_user'
+}).run(['$rootScope', 'consts', function ($rootScope, consts) {
+  $rootScope.consts = consts
+}])
