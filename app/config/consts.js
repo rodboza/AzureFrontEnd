951a5d2cd72538a12d6d51cb0df1fdb43a094dab
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-20 09:42:06 -0300

    Update consts.js

--- app/config/consts.js
+++ app/config/consts.js
@@ -6,8 +6,8 @@ angular.module('azurefrontend').constant('consts', {
   _owner: 'RODBOZA',
   _year: '2017',
   _site: 'http://rodboza.azurewebsites.net/',
-  _apiUrl: 'http://rodboza.backend.azurewebsites.net/api',
-  _oapiUrl: 'http://rodboza.backend.azurewebsites.net/oapi',
+  _apiUrl: 'http://rodbozabackend.azurewebsites.net/api',
+  _oapiUrl: 'http://rodbozabackend.azurewebsites.net/auth',
   _userKey: '_primeira_app_user'
 }).run(['$rootScope', 'consts', function ($rootScope, consts) {
   $rootScope.consts = consts
