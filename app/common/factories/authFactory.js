4ec76f393bb7a9528fe473e93be181d6c00080e9
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-20 09:49:08 -0300

    Update authFactory.js

--- app/common/factories/authFactory.js
+++ app/common/factories/authFactory.js
@@ -12,14 +12,17 @@ function AuthFactory($http, $rootScope, consts) {
     let user = null
 
     function signup(user, callback) {
-        submit('signup', user, callback)
+        console.log("call authFactory.js\signup");
+        submit('signup', user, callback);
     }
 
     function login(user, callback) {
-        submit('login', user, callback)
+        console.log("call authFactory.js\login");
+        submit('login', user, callback);
     }
 
     function submit(url, user, callback) {
+        console.log("call authFactory.js\sumbit");
         $http.post(`${consts._oapiUrl}/${url}`, user)
             .then(resp => {
                 localStorage.setItem(consts._userKey, JSON.stringify(resp.data))
@@ -27,37 +30,40 @@ function AuthFactory($http, $rootScope, consts) {
                 if (callback) callback(null, resp.data)
             }).catch(function (resp) {
                 if (callback) callback(resp.data.errors, resp.data)
-            })
+            });
     }
 
     function logout(callback) {
-        user = null
-        localStorage.removeItem(consts._userKey)
-        $http.defaults.headers.common.Authorization = ''
-        if (callback) callback(null)
+        console.log("call authFactory.js\logout");
+        user = null;
+        localStorage.removeItem(consts._userKey);
+        $http.defaults.headers.common.Authorization = '';
+        if (callback) callback(null);
     }
 
     function validateToken(token, callback) {
+        console.log("call authFactory.js\validateToken");
         if (token) {
             $http.post(`${consts._oapiUrl}/validateToken`, { token })
                 .then(resp => {
                     if (!resp.data.valid) {
-                        logout()
+                        logout();
                     } else {
-                        $http.defaults.headers.common.Authorization = getUser().token
+                        $http.defaults.headers.common.Authorization = getUser().token;
                     }
-                    if (callback) callback(null, resp.data.valid)
+                    if (callback) callback(null, resp.data.valid);
                 }).catch(function (resp) {
-                    if (callback) callback(resp.data.errors)
+                    if (callback) callback(resp.data.errors);
                 })
         } else {
-            if (callback) callback('Token inválido.')
+            if (callback) callback('Token inválido.');
         }
     }
 
     function getUser() {
+        console.log("call authFactory.js\getUser");
         if(!user) {
-            user = JSON.parse(localStorage.getItem(consts._userKey))
+            user = JSON.parse(localStorage.getItem(consts._userKey));
         }
         return user
     }
