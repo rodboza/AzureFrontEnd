2e9a082312b5c1e3fa7309d0c05c3d2d6a146c18
Rodrigo Barboza <rodboza@gmail.com>
2017-08-02 22:58:52 -0300

    teste

--- app/auth/authController.js
+++ app/auth/authController.js
@@ -8,24 +8,22 @@ angular.module('azurefrontend').controller('AuthCtrl', [
 ])
 
 function AuthController($location, auth, msgs) {
-    const vm = this;
-    vm.loginMode = true;
+    const vm = this
+    vm.loginMode = true
 
-    vm.path = process.env.PATH;
-
-    vm.changeMode = () => vm.loginMode = !vm.loginMode;
-    vm.getUser = () => auth.getUser();
+    vm.changeMode = () => vm.loginMode = !vm.loginMode
+    vm.getUser = () => auth.getUser()
 
     vm.login = () => {
-        auth.login(vm.user, err => err ? msgs.addError(err) : $location.path('/'));
+        auth.login(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
     }
 
     vm.signup = () => {
-        auth.signup(vm.user, err => err ? msgs.addError(err) : $location.path('/'));
+        auth.signup(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
     }
 
     vm.logout = () => {
-        auth.logout(() => $location.path('/'));
+        auth.logout(() => $location.path('/'))
     }
 }
 
