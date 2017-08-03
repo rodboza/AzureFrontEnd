06b1c556c3fe8d89b61ebbf711d4fb82fb9ef450
rodboza <rodboza@gmail.com>
2017-08-03 14:23:22 +0000

    teste com variavel ambient

--- app/auth/authController.js
+++ app/auth/authController.js
@@ -8,22 +8,23 @@ angular.module('azurefrontend').controller('AuthCtrl', [
 ])
 
 function AuthController($location, auth, msgs) {
-    const vm = this
-    vm.loginMode = true
+    const vm = this;
+    vm.loginMode = true;
+	vm.path = prosses.env.PATH || 'não achou o path';
 
-    vm.changeMode = () => vm.loginMode = !vm.loginMode
-    vm.getUser = () => auth.getUser()
+    vm.changeMode = () => vm.loginMode = !vm.loginMode;
+    vm.getUser = () => auth.getUser(0);
 
     vm.login = () => {
-        auth.login(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
+        auth.login(vm.user, err => err ? msgs.addError(err) : $location.path('/'));
     }
 
     vm.signup = () => {
-        auth.signup(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
+        auth.signup(vm.user, err => err ? msgs.addError(err) : $location.path('/'));
     }
 
     vm.logout = () => {
-        auth.logout(() => $location.path('/'))
+        auth.logout(() => $location.path('/'));
     }
 }
 
